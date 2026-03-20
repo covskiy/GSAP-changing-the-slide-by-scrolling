0de9b6bcb784382af2b6bca22a613028d52a77e1
Constant <constantine.ru@gmail.com>
2026-03-21 00:20:12 +0500

    Регистрация анимаций
    - добавление GSAP
    - написание кода timeline

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,58 @@
-import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from 'gsap/SplitText';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more<br>Такие дела, епта!
-    </p>
-  </div>
-`;
+document.addEventListener('DOMContentLoaded', () => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+  // Выключаем на мобильных браузерах сворачивание адресной строки при скролле
+  // в противном случае комбинирование dvh, svh, min-height и ScrollTrigger.refresh()
+  ScrollTrigger.normalizeScroll(true);
+
+  /* Text Split */
+  const textElements = document.querySelectorAll('.col-3 h2, .col-3 p');
+  textElements.forEach((el) => {
+    const split = new SplitText(el, {
+      type: 'lines',
+      linesClass: 'line',
+    });
+    split.lines.forEach((line) => {
+      line.innerHTML = `<span>${line.textContent}</span>`;
+    });
+  });
+
+  gsap.set('.col-3 .col-content-wrapper .line span', { y: '0%' });
+  gsap.set('.col-3 .col-content-wrapper-2 .line span', { y: '-125%' });
+
+  /* ---------------- Master Timeline --------------- */
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.sticky-cols',
+      start: 'top top',
+      end: '+=500%',
+      pin: true,
+      scrub: 1,
+      // markers: true,
+    },
+  });
+
+  /* ---------------- Step 1 ------------------------ */
+  tl.to('.col-1', { opacity: 0, scale: 0.75 })
+    .to('.col-2', { x: '0%' }, '<')
+    .to('.col-3', { y: '0%' }, '<')
+    .to('.col-img-1 img', { scale: 1.25 }, '<')
+    .to(
+      '.col-img-2',
+      { clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' },
+      '<',
+    )
+    .to('.col-img-2 img', { scale: 1 }, '<');
+
+  /* ---------------- Step 2 ------------------------ */
+  tl.to('.col-2', { opacity: 0, scale: 0.75 })
+    .to('.col-3', { x: '0%' }, '<')
+    .to('.col-4', { y: '0%' }, '<')
+    .to('.col-3 .col-content-wrapper .line span', { y: '-125%' }, '<')
+    .to('.col-3 .col-content-wrapper-2 .line span', { y: '0%' }, '<0.2');
+});
